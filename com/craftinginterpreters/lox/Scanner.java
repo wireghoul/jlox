e0f717b450a6550a44aefd89f8b9357947c6e823
Wireghoul <wireghoul@justanotherhacker.com>
2021-03-15 08:28:53 +1100

    Fixed warning about unchecked cast of arraylist to token list

--- com/craftinginterpreters/lox/Scanner.java
+++ com/craftinginterpreters/lox/Scanner.java
@@ -30,7 +30,7 @@ class Scanner {
     }
 
     private final String source;
-    private final List<Token> tokens = new ArrayList();
+    private final List<Token> tokens = new ArrayList<Token>();
     private int start = 0;
     private int current = 0;
     private int line = 1;
