45e046d9bd46713744634ae5ea88e0c85addc01f
Wireghoul <wireghoul@justanotherhacker.com>
2021-03-31 11:04:50 +1100

    Adding intepreter to make the code execute

---
+++ com/craftinginterpreters/lox/Interpreter.java
@@ -0,0 +1,54 @@
+package com.craftinginterpreters.lox;
+
+class Interpreter implements Expr.Visitor<Object> {
+
+
+    @Override public Object visitLiteralExpr(Expr.Literal expr) {
+        return expr.value;
+    }
+
+    @Override public Object visitGroupingExpr(Expr.Grouping expr) {
+        return evaluate(expr.expression);
+    }
+
+    private Object evaluate(Expr expr) {
+        return expr.accept(this);
+    }
+
+    @Override public Object visitUnaryExpr(Expr.Unary expr) {
+        Object right = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            case BANG:
+                return !isTruthy(right);
+            case MINUS:
+                return -(double)right;
+        }
+
+        // Unreachable.
+        return null;
+    }
+
+    @Override public Object visitBinaryExpr(Expr.Binary expr) {
+        Object left = evaluate(expr.left);
+        Object right = evaluate(expr.right); 
+
+        switch (expr.operator.type) {
+        case MINUS:
+            return (double)left - (double)right;
+        case SLASH:
+            return (double)left / (double)right;
+        case STAR:
+            return (double)left * (double)right;
+        }
+
+        // Unreachable.
+        return null;
+    }
+
+    private boolean isTruthy(Object object) {
+        if (object == null) return false;
+        if (object instanceof Boolean) return (boolean)object;
+        return true;
+    }
+}
